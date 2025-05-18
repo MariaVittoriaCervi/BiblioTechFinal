8f9a48cbb2f22df4c975f794b7d67fc7845d2a5d
irenemorgana <sirene.barezzi@itis.pr.it>
2025-05-18 16:23:22 +0200

    sistemato il percorso per la pag book

--- js/authors.js
+++ js/authors.js
@@ -19,7 +19,7 @@ function popolaLocations(data, container) {
         <td>${author.nationality}</td>
         <td>${author.sex}</td>
         <td>
-          <button onclick="window.location.href='view/author.html?id=${author.id_author}'">
+          <button onclick="window.location.href='author.html?id=${author.id_author}'">
             discover more
           </button>
         </td>
